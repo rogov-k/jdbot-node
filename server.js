dbd92eab8135ae9f8c75ce7ec59be20804e887bb
Kirill.R <pluskika@gmail.com>
2021-06-16 01:56:52 +0300

    Remove listening 3000 port

--- server.js
+++ server.js
@@ -1,10 +1,5 @@
 const app = require('./index')
 
-app.listen(3000, (err) => {
-    if (err) throw err
-    console.log('Server running in http://127.0.0.1:3000');
-})
-
-app.get('/', function(req, res){
+app.get('/', (req, res) => {
     res.sendfile(__dirname + '/index.html');
 });
