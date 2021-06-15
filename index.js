383f0ddf6ef713a7570677fd900953cc4f3ec9a3
Kirill.R <pluskika@gmail.com>
2021-06-16 02:08:29 +0300

    Add listening env port

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-module.exports = app
+module.exports = app;
--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-const app = require('./index')
+const app = require('./index');
+
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, () => {
+    console.log('Application has been started');
+});
 
 app.get('/', (req, res) => {
     res.sendfile(__dirname + '/index.html');
